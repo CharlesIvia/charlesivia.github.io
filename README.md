bb57c0c87115ba90eec58fdfacd43222bc589a6b
Charles Ivia <iviacharles@gmail.com>
2020-02-11 10:50:07 +0300

    Etch a Sketch files

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is part of The Odin Project Web Developemt curriculum. 
+
+In this project, I have created a wenb version of a sketchpad.
---
+++ javascript/index.js
@@ -0,0 +1,44 @@
+const mainContainer = document.querySelector(".sketchbox");
+const resetButton = document.querySelector("button");
+
+function createSquares(amount) {
+    mainContainer.style.gridTemplateColumns = `repeat(${amount}, 1fr)`;
+    mainContainer.style.gridTemplateRows = `repeat(${amount}, 1fr)`;
+
+    for(let i = 0; i < amount**2; i++) {
+        let square = document.createElement("div");
+        square.classList.add("item");
+        square.style.opacity = "1";
+        mainContainer.append(square);
+    }
+}
+
+
+function getRandomRGBValue() {
+    return Math.floor(Math.random() * 256);
+}
+
+function getRandomColor() {
+    return `rgb(${getRandomRGBValue()}, ${getRandomRGBValue()}, ${getRandomRGBValue()})`;
+}
+
+function resetSketchbox() {
+    mainContainer.innerHTML = "";
+    getBoardSize = prompt("Please enter the size for your sketch board");
+    createSquares(getBoardSize);
+}
+
+mainContainer.addEventListener('mouseover', (event) => {
+    currentTileColor = event.target.style.backgroundColor;
+    currentOpacity = event.target.style.opacity;
+
+    if(currentTileColor == "") {
+        event.target.style.backgroundColor = getRandomColor();
+    } else if(currentOpacity > 0) {
+        event.target.style.opacity = currentOpacity - 0.1;
+    }
+});
+
+resetButton.addEventListener("click", resetSketchbox);
+
+createSquares(10);
